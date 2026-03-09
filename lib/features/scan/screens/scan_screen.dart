2d064d99607db5431ad8cce55c711eb10cde0ab1
Sangeeta Gupta <sangeeta@pravaahconsulting.com>
2026-03-09 22:05:31 +0530

    feat: Added appbar in Scan screen

--- lib/features/scan/screens/scan_screen.dart
+++ lib/features/scan/screens/scan_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:payment_app/core/theme/text_stylies.dart';
 
 class ScanScreen extends StatefulWidget {
   const ScanScreen({super.key});
@@ -11,6 +13,15 @@ class _ScanScreenState extends State<ScanScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_back_ios_new, color: Colors.white),
+        ),
+        title: Text("Scan QR ", style: AppTextStyles.headingBlackTextStyle.copyWith(fontSize: 14.sp),),
+      ),
       body: Center(child: Text("Scan Screen")),
     );
   }
